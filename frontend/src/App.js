2b481da758de8f24fb80a5cf8cca16cadd575847
jjeancharles <jjeancharles@wesleyan.edu>
2022-04-19 03:04:11 -0400

    adding authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,71 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './Login.js';
 import Music from './Music.js';
 import Signup from './Signup.js';
 import NavigationBar from './components/Navbar';
+import PrivateRoute from "./components/common/PrivateRoute.js";
+import { loadUser } from "./actions/auth"
+import { Provider } from "react-redux";
+import store from "./store.js";
+import Dashboard from "./components/leads/Dashboard.js";
+import Alerts from "./components/layout/Alerts.js";
+import Register from "./components/accounts/Register.js";
+import PropTypes from 'prop-types'
+import * as  ReactDOM from "react-dom";
 
-export default function App() {
+
+// Alerts
+const alertOptions = {
+  timeout: 3000,
+  position: 'top center',
+};
+
+
+export default class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+  
+
+  render() {
     return (
-        <BrowserRouter>
+      <Provider store={store}>
+      <BrowserRouter>
         <NavigationBar></NavigationBar>
-          <Routes>
-            <Route exact path="/music" element={<Music />}></Route>
+        <Routes>
+          <Route exact path="/music" element={<Music />}></Route>
           <Route exact path="/login" element={<Login />}></Route>
-        <Route exact path="/createaccount" element={<Signup />}></Route>
-         </Routes>
-         </BrowserRouter>
-
-
-    );
+          <Route exact path="/createaccount" element={<Signup />}></Route>
+        </Routes>
+      </BrowserRouter>
+      </Provider>);
+  }
 }
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
+
+
+
+/*export default function App() {
+Component.componentDidMount(); {
+  store.dispatch(loadUser());
+}
+
+return (
+   <BrowserRouter>
+     <NavigationBar></NavigationBar>
+     <Routes>
+       <PrivateRoute exact path="/music" element={<Music />}></PrivateRoute>
+       <Route exact path="/login" element={<Login />}></Route>
+       <Route exact path="/createaccount" element={<Signup />}></Route>
+     </Routes>
+   </BrowserRouter>
+
+
+ );
+}
+*/
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,30 +1,127 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { connect } from "react-redux";
+import { PropTypes } from 'prop-types';
+import { render } from 'react-dom';
+import { logout } from '../actions/auth';
 
-function NavigationBar() {
-    return (
-        <div className="App">
-            <Navbar className="navbar" >
-                <Navbar.Brand href="#home">R8 UR MUSIC</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="navbartitle">
-                        <Nav.Link href="/home">Home</Nav.Link>
-                        <Nav.Link href="/login">Login</Nav.Link>
-                        <Nav.Link href="/music">Songs</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </div>
-    );
+/* function NavigationBar() {
+    propTypes = {
+        auth: propTypes.object.isRequired,
+        logout: PropTypes.func.isRequired
+    }
+
+    render(); {
+        const { isAuthenticated, user } = this.props.auth;
+
+        const authLinks = (
+            <div>
+                <span>
+                    <strong>
+                        {user ? `Welcome ${user.username}` : ""}
+                    </strong>
+                </span>
+                <button onClick={this.props.logout}>Logout</button>
+            </div>
+        )
+            
+
+        const guestLinks = (
+            <div>
+                <Nav.Link href="/login">Login</Nav.Link>
+                <Nav.Link href="/createaccount">Sign Up</Nav.Link>
+            </div>
+        )
+        return (
+            <div className="App">
+                <Navbar className="navbar" >
+                    <Navbar.Brand href="#home">R8 UR MUSIC</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="navbartitle">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            {isAuthenticated ? authLinks : guestLinks}
+
+                            <Nav.Link href="/music">Songs</Nav.Link>
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+        );
+    }
 }
 
-export default NavigationBar;
+const mapStateToProps = state => ({
+    auth: state.auth
+})
+
+export default connect(mapStateToProps, { logout })(NavigationBar);
+*/
+
+
+export class NavigationBar extends Component {
+    static propTypes = {
+        auth: PropTypes.object.isRequired,
+        logout: PropTypes.func.isRequired
+    };
+
+    render() {
+        const { isAuthenticated, user } = this.props.auth;
+
+        const authLinks = (
+            <div>
+                <span>
+                    <strong>
+                        {user ? `Welcome ${user.username}` : ""}
+                    </strong>
+                </span>
+                <button onClick={this.props.logout}>Logout</button>
+            </div>
+        )
+            
+
+        const guestLinks = (
+            <div>
+                <Nav.Link href="/login">Login</Nav.Link>
+                <Nav.Link href="/createaccount">Sign Up</Nav.Link>
+            </div>
+        )
+        return (
+            <div className="App">
+                <Navbar className="navbar" >
+                    <Navbar.Brand href="#home">R8 UR MUSIC</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="navbartitle">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            {isAuthenticated ? authLinks : guestLinks}
+
+                            <Nav.Link href="/music">Songs</Nav.Link>
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    auth: state.auth
+})
+
+export default connect(mapStateToProps, { logout })(NavigationBar);
