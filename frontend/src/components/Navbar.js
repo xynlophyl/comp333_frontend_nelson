a084f8f39f236cb306d7c473ac7766f845f9cbbf
jjeancharles <jjeancharles@wesleyan.edu>
2022-04-18 23:18:38 -0400

    navbar additions

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -13,6 +13,7 @@ function NavigationBar() {
                         <Nav.Link href="/home">Home</Nav.Link>
                         <Nav.Link href="/login">Login</Nav.Link>
                         <Nav.Link href="/music">Songs</Nav.Link>
+                        <Nav.Link href="/createaccount">Sign Up</Nav.Link>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
